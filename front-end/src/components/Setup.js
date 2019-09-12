37e3f39d8e322d87719db9f35d774c81635e9d4f
Sean Yim <superjjang89@gmail.com>
2019-09-12 11:15:32 -0400

    setup code for budget and name

--- front-end/src/components/Setup.js
+++ front-end/src/components/Setup.js
@@ -5,8 +5,19 @@ class Setup extends Component {
     render(){
         return (
             <div className="col col-sm-12">
-                
-                
+                <form onSubmit={this.submitSetup}>
+                    {/* bootstrap box */}
+                    <div className="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" className="form-control" id="name"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="total-budget">What is your budget?</label>
+                        <input type="number" className="form-control" id="total-budget"/>
+                    </div>
+
+                    
+                </form>
             </div>
         )
     }
