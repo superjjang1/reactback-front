ad15f7b27d2ea00b3e32f9b74e0564bda9e78605
Sean Yim <superjjang89@gmail.com>
2019-09-12 15:16:55 -0400

    questinging the user and setting up proper paths

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,14 +7,22 @@ import Navbar from './components/Navbar';
 
 
 class App extends React.Component {
+  state = {
+    name: ""
+  }
+  changeName = (name) =>{
+    this.setState({
+      name
+    })
+  }
   render(){
     return (
       <Router>
       <div className="container">
         <div className="row">
           <Route path="/" component={Navbar}/>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/setup" component={Setup}/>
+          <Route exact path="/" render={()=><Home changeName={this.changeName} name = {this.state.name}/>} />
+          <Route exact path="/setup" render={()=><Setup changeName={this.changeName} name={this.state.name}/>}/>
         </div>
       </div>
       </Router>
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -2,7 +2,21 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class Home extends Component {
+
     async componentDidMount(){
+        //if we don't know who the user is, ASK THEM!
+        if(!this.props.name){
+            const form = 
+            <form onSubmit={this.submitSetup}>
+                    {/* bootstrap box */}
+                    <div className="form-group">
+                        <label htmlFor="name">What is your name?</label>
+                        <input type="text" value={this.state.name} onChange={this.changeName} className="form-control" id="name"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    
+                </form>
+        }
         const getDudesUrl =`${window.apiUrl}/get-dudes`
         const dudeInfo = await axios.get(getDudesUrl);
         console.log(dudeInfo)
