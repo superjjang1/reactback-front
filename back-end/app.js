cf9ffced71a286ca338799034dc621034a79cfa8
Sean Yim <superjjang89@gmail.com>
2019-09-12 12:10:03 -0400

    added helmet

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const helmet = require('helmet');
+app.use(helmet());
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
