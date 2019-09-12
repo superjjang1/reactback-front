45ff66b52510d55a20f5794f245fba9b9ad75ba4
Sean Yim <superjjang89@gmail.com>
2019-09-12 11:11:03 -0400

    home skeleton, setup skeleton

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,7 +4,14 @@ import React, {Component} from 'react';
 class Home extends Component {
     render(){
         return (
-            <h1>Home</h1>
+            <div className="col col-sm-12">
+                <div className="total-budget">
+                    Total Budget: $11,000
+                </div>
+                <div className="total-spent">
+                    Total Spent: $550
+                </div>
+            </div>
         )
     }
 }
---
+++ front-end/src/components/Setup.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Setup extends Component {
+    render(){
+        return (
+            <div className="col col-sm-12">
+                
+                
+            </div>
+        )
+    }
+}
+
+export default Setup;
